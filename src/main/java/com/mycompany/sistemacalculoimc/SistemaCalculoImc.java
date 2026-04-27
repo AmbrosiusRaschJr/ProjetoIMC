ff8cbc632cc3d9eafb343b3fdbc0e374cf6e7b16
AmbrosiusRaschJr <ambrosiusjunior2008@gmail.com>
2026-04-27 14:58:01 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/sistemacalculoimc/SistemaCalculoImc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemacalculoimc;
+
+/**
+ *
+ * @author ambro
+ */
+public class SistemaCalculoImc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
